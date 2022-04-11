b56c3025292ac330036b8bc77fa2aa021ac2068a
OlegShoykhedenko <70909936+OlegShoykhedenko@users.noreply.github.com>
2022-04-10 23:00:11 -0700

    Reworked update functionality, added like/dislike methods, fixed exception status for incorrectly provided id

--- src/main/java/com/example/demo/movie/MovieController.java
+++ src/main/java/com/example/demo/movie/MovieController.java
@@ -1,7 +1,6 @@
 package com.example.demo.movie;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,16 +32,31 @@ public class MovieController {
         movieService.deleteMovie(movieId);
     }
 
-    @PutMapping(path = "/update/{movieId}")
-    public void updateMovie(
-            @PathVariable("movieId") Long movieID,
-            @RequestParam(required = false) String title,
-            @RequestParam(required = false) String description,
-            @RequestParam(required = false) Integer releaseYear,
-            @RequestParam(required = false) Double rating,
-            @RequestParam(required = false) Integer likeCount,
-            @RequestParam(required = false) Integer dislikeCount ) {
-        movieService.updateMovie(movieID, title, description, releaseYear, rating, likeCount, dislikeCount);
+//    @PutMapping(path = "/update/{movieId}")
+//    public void updateMovie(
+//            @PathVariable("movieId") Long movieID,
+//            @RequestParam(required = false) String title,
+//            @RequestParam(required = false) String description,
+//            @RequestParam(required = false) Integer releaseYear,
+//            @RequestParam(required = false) Double rating,
+//            @RequestParam(required = false) Integer likeCount,
+//            @RequestParam(required = false) Integer dislikeCount ) {
+//        movieService.updateMovie(movieID, title, description, releaseYear, rating, likeCount, dislikeCount);
+//    }
+
+    @PatchMapping("/update/{movieId}")
+    public void updateMovie(@RequestBody Movie movie, @PathVariable Long movieId) {
+        movieService.updateMovie(movie, movieId);
+    }
+
+    @PatchMapping("/like/{movieId}")
+    public void likeMovie(@PathVariable Long movieId) {
+        movieService.likeMovie(movieId);
+    }
+
+    @PatchMapping("/dislike/{movieId}")
+    public void dislikeMovie(@PathVariable Long movieId) {
+        movieService.dislikeMovie(movieId);
     }
 
 }
--- src/main/java/com/example/demo/movie/MovieService.java
+++ src/main/java/com/example/demo/movie/MovieService.java
@@ -1,10 +1,10 @@
 package com.example.demo.movie;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
-import javax.transaction.Transactional;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Objects;
 
@@ -25,49 +25,118 @@ public class MovieService {
     public void deleteMovie(Long movieId) {
         boolean exists = movieRepository.existsById(movieId);
         if(!exists) {
-            throw new IllegalStateException(
-                    "movie with id " + movieId + " does not exists"
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "movie with id " + movieId + " does not exists"
             );
         }
         movieRepository.deleteById(movieId);
     }
 
-    @Transactional
-    public void updateMovie(Long movieID,
-                            String title,
-                            String description,
-                            Integer releaseYear,
-                            Double rating,
-                            Integer likeCount,
-                            Integer dislikeCount) {
-        Movie movie = movieRepository.findById(movieID)
-                .orElseThrow(() -> new IllegalStateException(
-                        "movie with id " + movieID + " does not exists"
+    public void updateMovie(Movie movie, Long movieId) {
+        Movie updatedMovie = movieRepository.findById(movieId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "movie with id " + movieId + " does not exists"
                 ));
 
-        if(title != null && title.length() > 0 && !Objects.equals(movie.getTitle(), title)) {
-            movie.setTitle(title);
+        boolean needUpdate = false;
+        if(movie.getTitle() != null && movie.getTitle().length() > 0
+                && !Objects.equals(movie.getTitle(), updatedMovie.getTitle())) {
+            updatedMovie.setTitle(movie.getTitle());
+            needUpdate = true;
         }
 
-        if(description != null && description.length() > 0 && !Objects.equals(movie.getDescription(), description)) {
-            movie.setDescription(description);
+        if(movie.getDescription() != null && movie.getDescription().length() > 0
+                && !Objects.equals(movie.getDescription(), updatedMovie.getDescription())) {
+            updatedMovie.setDescription(movie.getDescription());
+            needUpdate = true;
         }
 
-        if(releaseYear != null &&
-                releaseYear >= 1895 &&
-                releaseYear <= Calendar.getInstance().get(Calendar.YEAR) &&
-                !Objects.equals(movie.getReleaseYear(), releaseYear)) {
-            movie.setReleaseYear(releaseYear);
+        if(movie.getReleaseYear() != null
+                && !Objects.equals(movie.getReleaseYear(), updatedMovie.getReleaseYear())) {
+            updatedMovie.setReleaseYear(movie.getReleaseYear());
+            needUpdate = true;
         }
 
-        if(rating != null && rating >= 0 && !Objects.equals(movie.getRating(), rating))
-            movie.setRating(rating);
+        if(movie.getRating() != null && movie.getRating() >= 0 &&
+                !Objects.equals(movie.getRating(), updatedMovie.getRating())) {
+            updatedMovie.setRating(movie.getRating());
+            needUpdate = true;
+        }
 
-        if(likeCount != null && likeCount >= 0 && !Objects.equals(movie.getLikeCount(), likeCount))
-            movie.setLikeCount(likeCount);
+        if(movie.getLikeCount() != null && movie.getLikeCount() >= 0
+                && !Objects.equals(movie.getLikeCount(), updatedMovie.getLikeCount())) {
+            updatedMovie.setLikeCount(movie.getLikeCount());
+            needUpdate = true;
+        }
 
-        if(dislikeCount != null && dislikeCount >= 0 && !Objects.equals(movie.getDislikeCount(), dislikeCount))
-            movie.setDislikeCount(dislikeCount);
+        if(movie.getDislikeCount() != null && movie.getDislikeCount() >= 0
+                && !Objects.equals(movie.getDislikeCount(), updatedMovie.getDislikeCount())) {
+            updatedMovie.setDislikeCount(movie.getDislikeCount());
+            needUpdate = true;
+        }
+
+        if(needUpdate) {
+            movieRepository.save(updatedMovie);
+        }
+
     }
+
+    public void likeMovie(Long movieId) {
+        Movie movie = movieRepository.findById(movieId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "movie with id " + movieId + " does not exists"
+                ));
+
+        movie.setLikeCount(movie.getLikeCount() + 1);
+        movieRepository.save(movie);
+    }
+
+    public void dislikeMovie(Long movieId) {
+        Movie movie = movieRepository.findById(movieId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "movie with id " + movieId + " does not exists"
+                ));
+
+        movie.setDislikeCount(movie.getDislikeCount() + 1);
+        movieRepository.save(movie);
+    }
+
+//    @Transactional
+//    public void updateMovie(Long movieID,
+//                            String title,
+//                            String description,
+//                            Integer releaseYear,
+//                            Double rating,
+//                            Integer likeCount,
+//                            Integer dislikeCount) {
+//        Movie movie = movieRepository.findById(movieID)
+//                .orElseThrow(() -> new IllegalStateException(
+//                        "movie with id " + movieID + " does not exists"
+//                ));
+//
+//        if(title != null && title.length() > 0 && !Objects.equals(movie.getTitle(), title)) {
+//            movie.setTitle(title);
+//        }
+//
+//        if(description != null && description.length() > 0 && !Objects.equals(movie.getDescription(), description)) {
+//            movie.setDescription(description);
+//        }
+//
+//        if(releaseYear != null &&
+//                releaseYear >= 1895 &&
+//                releaseYear <= Calendar.getInstance().get(Calendar.YEAR) &&
+//                !Objects.equals(movie.getReleaseYear(), releaseYear)) {
+//            movie.setReleaseYear(releaseYear);
+//        }
+//
+//        if(rating != null && rating >= 0 && !Objects.equals(movie.getRating(), rating))
+//            movie.setRating(rating);
+//
+//        if(likeCount != null && likeCount >= 0 && !Objects.equals(movie.getLikeCount(), likeCount))
+//            movie.setLikeCount(likeCount);
+//
+//        if(dislikeCount != null && dislikeCount >= 0 && !Objects.equals(movie.getDislikeCount(), dislikeCount))
+//            movie.setDislikeCount(dislikeCount);
+//    }
 
 }
